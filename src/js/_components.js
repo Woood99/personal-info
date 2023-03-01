c203120a2e3eb6a5180f1c32f54ae5134861b3f1
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2023-03-02 00:29:28 +0300

    finished

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,18 +1,25 @@
-            
-            
-            
-            
-            const getData = (url) =>
-                new Promise((resolve, reject) =>
-                    fetch(url)
-                    .then(response => response.json())
-                    .then(json => resolve(json))
-                    .catch(error => reject(error))
-                )
-            
-            
-            
-            getData('./products.json')
-                .then(data => console.log(data))
-                .catch(error => console.log(error))
-            
+        // const getData = (url) =>
+        //     new Promise((resolve, reject) =>
+        //         fetch(url)
+        //         .then(response => response.json())
+        //         .then(json => resolve(json))
+        //         .catch(error => reject(error))
+        //     )
+
+        // getData('./products.json').then(data => console.log(data)).catch(error => console.log(error.message));
+
+
+
+
+
+        
+
+        const getData = async (url) => {
+            const response = await fetch(url);
+            const json = await response.json();
+            return json;
+        }
+
+        getData('./products.json')
+            .then(array => console.log(array))
+            .catch(error => console.log(error.message))
