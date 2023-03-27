3291362f92d5ebedac6675f1dedff8096b8752d7
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2023-03-27 16:17:59 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -7,11 +7,7 @@
   \*******************************/
 /***/ (() => {
 
-const array = [2, 5, 6, 8, 2];
-const array2 = array.map(function (el) {
-  console.log(this);
-});
-console.log(array2);
+
 
 /***/ }),
 
--- src/js/_components.js
+++ src/js/_components.js
@@ -1,12 +1,3 @@
                 
                 
-                
-                
-                
-                const array = [2, 5, 6, 8, 2];
 
-                const array2 = array.map(function (el) {
-                    console.log(this);
-                })
-
-                console.log(array2);
